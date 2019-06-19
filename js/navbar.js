7c253edafa60a74bd3486e401665b3448772bcc6
Filipbagen <filip.bagen@gmail.com>
2019-06-19 09:34:47 +0200

    removed files
    removed some js-files

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-var nav = true
-function navbar () {
-    if (nav) {
-      document.getElementById('navcontainer').style.height = 'auto'
-    } else {
-      document.getElementById('navcontainer').style.height = '0'
-    }
-    nav =! nav
-}
